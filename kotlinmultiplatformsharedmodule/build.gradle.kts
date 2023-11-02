e12505568f1c89b884cce054729c483befc85c86
Maurizio Pietrantuono <maurizio.pietrantuono@gmail.com>
2023-11-02 21:49:52 +0000

    Fixed

--- kotlinmultiplatformsharedmodule/build.gradle.kts
+++ kotlinmultiplatformsharedmodule/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.com.android.library)
+    id("kotlinx-serialization")
 }
 
 //@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
